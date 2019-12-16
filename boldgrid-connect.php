41699097d6e187fa814d79d293c31d39983b51c7
Rafael Ramos <rramo012@gmail.com>
2019-12-16 12:32:53 -0500

    Adding meta box to inspirations screen

--- boldgrid-connect.php
+++ boldgrid-connect.php
@@ -74,3 +74,19 @@ function run_boldgrid_connect() {
 }
 
 run_boldgrid_connect();
+
+add_action( 'add_meta_boxes_admin_page_my-inspiration', function() {
+	add_meta_box(
+		'current_inspiration',
+		esc_html__( 'Publish Website', 'boldgrid-inspirations' ),
+		function () { ?>
+			<p>You've deployed this site on a development environment. To make
+			this website public, you'll need to transfer to a production environment.
+			Head back over to WordPress Central when you're done making changes to deploy your website.</p>
+			<a href="#" class="button button-primary">Publish Site</a>
+		<?php
+		},
+		'admin_page_my-inspiration',
+		'container6'
+	);
+}, 99 );
